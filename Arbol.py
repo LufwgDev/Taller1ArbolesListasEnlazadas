762158a3c39d5f48da27fb418dd26c7d44df3692
Manu2831 <jmanuelitort@gmail.com>
2025-04-11 22:52:46 -0500

    feat:Casi terminado

--- Arbol.py
+++ Arbol.py
@@ -17,7 +17,38 @@ class Arbol:
                 
     def Imprimir(self):
         self.ImprimirNivel(self.root)
-
+        
+    def Contador(self,nodo):
+        contador = 1
+        if not nodo.sons:
+            return contador
+        else:
+            for son in nodo.sons:
+                contador += self.Contador(son)
+            return contador
+    
+    def peso(self):
+        return self.Contador(self.root)
+    
+    def CalcularAltura(self,nodo):
+        if not nodo.sons:
+            return 0
+        else:
+            return 1 + max(self.CalcularAltura(h) for h in nodo.sons)
+        
+    def Altura(self):
+       return self.CalcularAltura(self.root)
+    
+    def calcularOrden(self,nodo):
+        orden = len(nodo.sons)
+        for son in nodo.sons:
+            orden = max(orden,self.calcularOrden(son))
+        return orden
+    
+    def orden(self):
+        return self.calcularOrden(self.root)
+                
+            
 root = Node("A")
 node_b = Node("B")
 node_c = Node("C")
@@ -37,3 +68,6 @@ node_e.addSon(node_h)
 
 arbol = Arbol(root)
 arbol.Imprimir()
+print("peso:",arbol.peso())
+print("Altura:",arbol.Altura())
+print("Orden:",arbol.orden())
